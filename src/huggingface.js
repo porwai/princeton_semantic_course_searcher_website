af26c590e6985ae16e85c940cee1aed969e98fab
porwai <porwaiwit@gmail.com>
2023-09-12 23:37:18 -0400

    Utilized FastAPI to connect knn searcher with the React Frontend

---
+++ src/huggingface.js
@@ -0,0 +1,29 @@
+/*
+
+import { HfInference } from "@Huggingeface/inference";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// HuggingFace access token
+const HF_ACESS_TOKEN = process.env.HF_ACESS_TOKEN // replace with access token
+
+// Initiation the huggging face inference class 
+const inference = new HfInference(HF_ACESS_TOKEN);
+
+// 4. Define the model and what to capture 
+const model 
+
+// 5. Fetch the image as a blocb
+var response = "hie"; // Replace with the manually entered text from the search bar
+
+// 6. sentence similarity function 
+
+var result = await inference.imageToText({
+    data: imageBlob, 
+    model: module
+});
+
+// conducts an knn search of the entire data base and returns the top 20 results. 
+
+*/
